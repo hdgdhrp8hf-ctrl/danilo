b3faba15ad17c20771e75282eea9b07443dbbe87
hdgdhrp8hf-ctrl <hdgdhrp8hf@privaterelay.appleid.com>
2026-08-24 22:13:01 +0200

    Format build.gradle.kts for consistency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,23 +3,32 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
 }
+
 android {
-    namespace="it.tifototitrovo"
-    compileSdk=36
+    namespace = "it.tifototitrovo"
+    compileSdk = 36
+
     defaultConfig {
-        applicationId="it.tifototitrovo"
-        minSdk=23
-        targetSdk=36
-        versionCode=1
-        versionName="0.1.0"
+        applicationId = "it.tifototitrovo"
+        minSdk = 23
+        targetSdk = 36
+        versionCode = 1
+        versionName = "0.1.0"
     }
-    kotlin { jvmToolchain(17) }
+
+    kotlin {
+        jvmToolchain(17)
+    }
 }
+
 dependencies {
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.activity:activity-compose:1.12.0")
+
+    implementation(platform("androidx.compose:compose-bom:2025.08.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+
     implementation("com.google.mlkit:image-labeling:17.0.9")
 }
